5b80c10d2e72d04f933bc9a08a7ced9cb0779e15
MHKEND01 <47224722+MHKEND01@users.noreply.github.com>
2019-02-21 15:43:14 -0500

    Create question3.js

---
+++ question3.js
@@ -0,0 +1,2 @@
+
+d3.select(".page-content-block").select(".field").select('.field-items').select(".field-item").select('ul').selectAll('li').select('a').nodes().forEach(function(e){console.log(e.innerText);})
