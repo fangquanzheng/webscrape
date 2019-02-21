a27a48609d171b0a4223b475b205e8c7daa346ed
MHKEND01 <47224722+MHKEND01@users.noreply.github.com>
2019-02-21 15:52:45 -0500

    Update question3.js

--- question3.js
+++ question3.js
@@ -3,3 +3,5 @@ s.src="https://d3js.org/d3.v5.js";
 var body = document.getElementsByTagName("body")[0];
 body.after(s);
 d3.select(".page-content-block").select(".field").select('.field-items').select(".field-item").select('ul').selectAll('li').select('a').nodes().forEach(function(e){console.log(e.innerText);});
+
+/*https://college.harvard.edu/academics/fields-study/concentrations */
