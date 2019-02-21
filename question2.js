aae2c0b7df2e025ca9314291fde935f87f041892
MHKEND01 <47224722+MHKEND01@users.noreply.github.com>
2019-02-21 15:30:10 -0500

    Update question2.js

--- question2.js
+++ question2.js
@@ -1,1 +1,5 @@
-
+var s = document.createElement("script");
+s.src="https://d3js.org/d3.v5.js";
+var body = document.getElementsByTagName("body")[0];
+body.after(s);
+d3.select("#main").select("ul").selectAll('li').select('a').nodes().forEach(function(e){console.log(e.innerText);});
