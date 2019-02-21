6ea9feb2515cc7cd21ab44f8c1fc92cefca9d615
MHKEND01 <47224722+MHKEND01@users.noreply.github.com>
2019-02-21 15:24:06 -0500

    Create question1.js

---
+++ question1.js
@@ -0,0 +1,5 @@
+var s = document.createElement("script")
+s.src="https://d3js.org/d3.v5.js"
+var body = document.getElementsByTagName("body")[0]
+body.after(s);
+d3.selectAll(".fusion-fullwidth").select(".fusion-builder-row").selectAll(".fusion-layout-column").select('.fusion-column-wrapper').select(".fusion-text").select("p").selectAll("a").nodes().forEach(function(e){console.log(e.innerText);})
