6dff90315f765407d5792ac1a770aa197a3c9667
MHKEND01 <47224722+MHKEND01@users.noreply.github.com>
2019-02-21 15:43:36 -0500

    Update question3.js

--- question3.js
+++ question3.js
@@ -1,2 +1,5 @@
-
-d3.select(".page-content-block").select(".field").select('.field-items').select(".field-item").select('ul').selectAll('li').select('a').nodes().forEach(function(e){console.log(e.innerText);})
+var s = document.createElement("script");
+s.src="https://d3js.org/d3.v5.js";
+var body = document.getElementsByTagName("body")[0];
+body.after(s);
+d3.select(".page-content-block").select(".field").select('.field-items').select(".field-item").select('ul').selectAll('li').select('a').nodes().forEach(function(e){console.log(e.innerText);});
