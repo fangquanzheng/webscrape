7b5448cb979d06dd10bb70e2f40d4834da7bed25
MHKEND01 <47224722+MHKEND01@users.noreply.github.com>
2019-02-21 15:54:15 -0500

    Update question2.js

--- question2.js
+++ question2.js
@@ -3,3 +3,5 @@ s.src="https://d3js.org/d3.v5.js";
 var body = document.getElementsByTagName("body")[0];
 body.after(s);
 d3.select("#main").select("ul").selectAll('li').select('a').nodes().forEach(function(e){console.log(e.innerText);});
+
+/*http://centre.smartcatalogiq.com/2018-2019/Catalog/Course-Descriptions/CSC-Computer-Science */
