4e1d423b0f6cf1f0558c9a414c93c3eca4166baa
MHKEND01 <47224722+MHKEND01@users.noreply.github.com>
2019-02-21 15:51:37 -0500

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,2 +1,7 @@
-
+var s = document.createElement("script");
+s.src="https://d3js.org/d3.v5.js";
+var body = document.getElementsByTagName("body")[0];
+body.after(s);
 d3.select("tbody").selectAll('tr').select('.column-1').select('p').select('a').nodes().forEach(function(e){console.log(e.innerText);});
+
+/*https://www.berea.edu/academics/majors-minors/*/
